7de26a4fadfcb8d6074e410ed5c4208b689c0473
Daniel Ménard <daniel.menard@laposte.net>
2018-02-03 12:46:49 +0100

    Nouveau champ Organization/Name

---
+++ class/Field/Organization/Name.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\People\Field\Organization;
+
+use Docalist\Type\TypedText;
+
+/**
+ * Noms de l'organisme.
+ *
+ * Ce champ permet d'indiquer les différents noms de la structure (nom usuel, sigle, ancien nom...)
+ *
+ * Chaque date comporte deux sous-champs :
+ * - `type` : type de nom,
+ * - `value` : nom.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les types de noms disponibles
+ * ("table:organization-name" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Name extends TypedText
+{
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Nom', 'docalist-people'),
+            'description' => __('Nom ou sigle de la structure.', 'docalist-people'),
+            'fields' => [
+                'type' => [
+                    'table' => 'table:organization-name',
+                ],
+                'value' => [
+                    'label' => __('Nom', 'docalist-people'),
+                ],
+            ],
+            'default' => [['type' => 'usual']],
+        ];
+    }
+}
