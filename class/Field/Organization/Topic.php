228b26242822c46b1a30c66bb40fc2f9f9b84a76
Daniel Ménard <daniel.menard@laposte.net>
2018-02-03 12:50:10 +0100

    Nouveau champ Organization/Topic

---
+++ class/Field/Organization/Topic.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\People\Field\Organization;
+
+use Docalist\Data\Type\Topic as BaseTopic;
+
+/**
+ * Mots-clés décrivant l'organisme.
+ *
+ * Ce champ permet de saisir des mots-clés provenant de différents vocabulaires pour décrire et classer la structure.
+ *
+ * Chaque indexation comporte deux sous-champs :
+ * - `type` : vocabulaire,
+ * - `value` : mots-clés.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui liste les types d'indexation disponibles
+ * ("table:topics" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Topic extends BaseTopic
+{
+    public static function loadSchema()
+    {
+        return [
+            'description' => __('Mots-clés permettant de décrire et de classer la structure.', 'docalist-people'),
+        ];
+    }
+}
