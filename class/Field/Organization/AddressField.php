f389e651ecb825e0f0485c8ba7572aeb0ab8080c
Daniel Ménard <daniel.menard@laposte.net>
2018-02-07 17:38:36 +0100

    Nouveau champ org/AddressField (hérite juste du champ address standard)

---
+++ class/Field/Organization/AddressField.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\People\Field\Organization;
+
+use Docalist\Data\Field\AddressField as BaseAddressField;
+
+/**
+ * Champ "address" pour les entités organization.
+ *
+ * Cette classe hérite simplement du champ standard de docalist-data et modifie les paramètres par défaut.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class AddressField extends BaseAddressField
+{
+}
