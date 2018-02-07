c59fc68763488d12426b64c10746a814207de104
Daniel Ménard <daniel.menard@laposte.net>
2018-02-07 19:05:07 +0100

    Classe principale du plugin docalist-people

---
+++ class/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\People;
+
+use Docalist\People\Entity\OrganizationEntity;
+
+/**
+ * Plugin docalist-people.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Plugin
+{
+    /**
+     * Initialise le plugin.
+     */
+    public function __construct()
+    {
+        // Déclare les entités définies dans ce plugin
+        add_filter('docalist_databases_get_types', function (array $types) {
+            return $types + [
+                'organization' => OrganizationEntity::class,
+            ];
+        });
+    }
+}
