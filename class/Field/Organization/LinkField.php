bcb0f372e911b3a53e4aca68075aee689f09fc39
Daniel Ménard <daniel.menard@laposte.net>
2018-02-07 17:39:50 +0100

    Nouveau champ org/LinkField (hérite juste du champ link standard)

---
+++ class/Field/Organization/LinkField.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\People\Field\Organization;
+
+use Docalist\Data\Field\LinkField as BaseLinkField;
+
+/**
+ * Champ "link" pour les entités organization.
+ *
+ * Cette classe hérite simplement du champ standard de docalist-data et modifie les paramètres par défaut.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class LinkField extends BaseLinkField
+{
+}
