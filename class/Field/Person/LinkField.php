7e6e3dba893b458127a2bb035fb0937cec66f135
Daniel Ménard <daniel.menard@laposte.net>
2019-02-07 10:24:23 +0100

    Nouvelle classe LinkField : champ link pour les personnes

---
+++ class/Field/Person/LinkField.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Docalist People.
+ *
+ * Copyright (C) 2017-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\People\Field\Person;
+
+use Docalist\Data\Field\LinkField as BaseLinkField;
+
+/**
+ * Champ "link" pour les entités "person".
+ *
+ * Cette classe hérite simplement du champ standard de docalist-data et modifie les paramètres par défaut.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class LinkField extends BaseLinkField
+{
+}
